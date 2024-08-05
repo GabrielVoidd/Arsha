d90fcbfefaefe6e45da46d33e76464312837da1d
Gabriel <souzagabriel0006@gmail.com>
2024-08-05 20:47:49 -0300

    upload do projeto

---
+++ galeria/migrations/urls.py
@@ -0,0 +1,14 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        # Dependências de migração anteriores, por exemplo, ('galeria', '0001_initial'),
+    ]
+
+    operations = [
+        # Operações de migração, como AddField, RemoveField, etc.
+    ]
+
+
